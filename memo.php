c122d489a99822eddb88ba31913dd6a04df73fcd
adonnart <alexandre.donnart@gmail.com>
2017-04-04 09:23:18 +0200

    Create memo.php

---
+++ memo.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+
+> Visionner par : Jour | Semaine | Mois | Annee       Modifier le planning
+
+-----------------------------------------------------------------------------------------------------------------------
+MON PLANNING | MON COMPTE | MA COMPTABILITE | EVENEMENTS A VENIR | COLLABORATION | SMART ASSISTANT
+-----------------------------------------------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------------------------------------------
++ MON PLANNING
+-----------------------------------------------------------------------------------------------------------------------
+
+Nombre d'heures programmées : $
+
+Créneaux libres laissés aux remplaçants : $
+
+Revenu net moyen potentiel sans les remplacements : $
+
+Taux de remplissage : $
+
+-----------------------------------------------------------------------------------------------------------------------
++ MON COMPTE
+-----------------------------------------------------------------------------------------------------------------------
+
+> Modifier ma photo
+
+> Modifier mes infos
+
+> Changer mon mot de passe
+
+| Nom :
+| Prénom :
+| Adresse :
+|
+| Mail :
+| Téléphone :
+|
+| Age :
+|
+| Rôle chez W'Health Come :
+| Chez W'Health Come depuis :
+|
+| Spécialité :
+|
+| Remplaçants sous contrat avec le médecin :
+| Remplaçants parrainés par le médecin :
+
+-----------------------------------------------------------------------------------------------------------------------
++ MA COMPTABILITE
+-----------------------------------------------------------------------------------------------------------------------
+
+Reversement individuel sur le mois en cours : $
+
+Reversement individuel cumulé sur l'année en cours : $
+
+Gain du cabinet : $
+
+Gamification (nombre de points cumulés) : $
+
+-----------------------------------------------------------------------------------------------------------------------
++ EVENEMENTS A VENIR
+-----------------------------------------------------------------------------------------------------------------------
+
+Prochaines vacances programmées : "date" ---> J-200 (calcul)
+
+Taux de remplissage M+1 individuel : **%
+
+Taux de remplissage M+1 collectif : **%
+
+Prochain remplacement : Dr LEBLAIS Marc     Le contacter : Mail | Portable (liens)
+
+Envoyer un message | Liste des contacts
+
+-----------------------------------------------------------------------------------------------------------------------
++ COLLABORATION
+-----------------------------------------------------------------------------------------------------------------------
+
+> Tapez ici l'info que vous souhaitez communiquer à l'ensemble du cabinet.
+
+> Tapez ici les infos et directives à donner aux assistants.
+
+-----------------------------------------------------------------------------------------------------------------------
++ SMART ASSISTANT
+-----------------------------------------------------------------------------------------------------------------------
+
+> Pensez à valider votre planning du mois de mars avant le 31 janvier !
+
+*/
